086cf606301d71a742a9209114098cd7e2905cbc
Allan Pead <adpead@gmail.com>
2020-08-28 21:31:24 +0200

    Initial Commit

---
+++ Scripts/setupenv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install xrdp -y
+sudo apt-get install xfce4 -y
+sudo echo xfce4-session >/root/.xsession
+sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
+sudo service xrdp restart
+
+#Dotnetsdk
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+#VS Code
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
