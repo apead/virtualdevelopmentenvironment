1d50e1a82e0d427681fb44603015d47ee9be9abb
Allan Pead <adpead@gmail.com>
2020-08-29 18:22:02 +0200

    Updated script

--- Scripts/setupenv.sh
+++ Scripts/setupenv.sh
@@ -16,11 +16,11 @@ sudo apt-get update; \
 
 #VS Code
 sudo apt update
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
-sudo apt install code
+sudo apt install code -y
 
-sudo apt install git-all
+
