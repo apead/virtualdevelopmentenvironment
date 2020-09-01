4590550d5988c78f64ce36d47fa170287cfcff97
Allan Pead <adpead@gmail.com>
2020-09-01 06:26:48 +0200

    update

--- Scripts/setupenv.sh
+++ Scripts/setupenv.sh
@@ -3,11 +3,13 @@ sudo apt-get update
 #install gnome desktop
 #sudo apt-get install ubuntu-desktop -y
 #install xrdp
-sudo apt-get install xorgxrdp-hwe-18.04 -y
-#sudo apt-get install xrdp -y
+#sudo apt-get install xorgxrdp-hwe-18.04 -y
+sudo apt-get install xfce4 -y
+sudo apt-get install xrdp -y
+sudo systemctl enable xrdp
 
 #using xfce if you are using Ubuntu version later than Ubuntu 12.04LTS
-sudo apt-get install xfce4 -y
+
 #sudo apt-get install xubuntu-desktop -y
 sudo echo xfce4-session >/root/.xsession
 sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
